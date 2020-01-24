f47d05cb7b045550bdee7521e816f30f2151024f
Filip Reimer <filip.reimer@ikea.com>
2020-01-24 13:13:50 +0100

    Modulename

--- src/terraform_plan.sh
+++ src/terraform_plan.sh
@@ -5,7 +5,7 @@ function terraformPlan {
   echo "plan: info: planning Terraform configuration in ${tfWorkingDir}"
   if [ "${tfWorkingDirLoop}" != "" ]; then
     EXITCODE=0
-    planOutput="$( for dir in  ${tfWorkingDirLoop}/*/; do echo $dir; (cd $dir; terraform plan -detailed-exitcode -input=false ${*} 2>&1|| exit $?)||EXITCODE=$?;done; exit ${EXITCODE} )"
+    planOutput="$( for dir in  ${tfWorkingDirLoop}/*/; do TF_MODULENAME=${dir}; echo $dir; (cd $dir; terraform plan -detailed-exitcode -input=false ${*} -out 2>&1|| exit $?)||EXITCODE=$?;done; exit ${EXITCODE} )"
     planExitCode=${?}
   else
     planOutput=$(terraform plan -detailed-exitcode -input=false ${*} 2>&1)
